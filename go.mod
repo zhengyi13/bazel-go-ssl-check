59c58b1b1e3bae21d5ba4a4bb34a061198013a25
Justin M <zhengyi13@gmail.com>
2021-02-19 11:20:09 -0800

    move probe() to an external module

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zhengyi13/bazel-go-ssl-check
 
 go 1.15
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/zhengyi13/prober v0.0.0-20210219184752-1832e984ae19
+	gopkg.in/yaml.v2 v2.4.0
+)
--- ssl_expiry.go
+++ ssl_expiry.go
@@ -1,44 +1,19 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
 	yaml "gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"github.com/zhengyi13/prober"
 )
 
-// Basic type is a HostPort string like "localhost:8443"
-type HostPort string
-
 // Probes is a list of HostPorts
-type Probes []HostPort
+type Probes []prober.HostPort
 
 // And a ProbeConfig struct is a mapping of a Probes list to the  "probes:" heading in a YAML config
 type ProbeConfig struct {
 	Probes `yaml:"probes"`
-}
-
-func probe(hp HostPort) (timestamp int64, err error) {
-	// Given a HostPort, attempt to connect via SSL.
-	// On failure, return an error
-	// On success, return the certificate's expiration date as a Unix timestamp
-	config := tls.Config{
-		InsecureSkipVerify: true,
-	}
-	conn, err := tls.Dial("tcp", string(hp), &config)
-	if err != nil {
-		log.Printf("ERROR dial: %s\n", err)
-		return timestamp, err
-	}
-	defer conn.Close()
-	log.Printf("client: connected to %s\n ", conn.RemoteAddr())
-	s := conn.ConnectionState()
-	for _, cert := range s.PeerCertificates {
-		log.Printf("CN: %s\n", cert.Subject)
-		timestamp = cert.NotAfter.Unix() // thus we naturally always get the last timestamp
-	}
-	return timestamp, err
 }
 
 func main() {
@@ -58,7 +33,7 @@ func main() {
 	log.Printf("--- config:\n%v\n\n", config)
 	for i, hp := range config.Probes {
 		log.Printf("Probe %d: %s\n", i, hp)
-		ts, err := probe(hp)
+		ts, err := prober.Probe(hp)
 		if err != nil {
 			log.Printf("ERROR Failed to probe %s\n", hp)
 		}
